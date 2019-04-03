e788d755102fb7f75ebe0c04506293f901aba9f9
dolmengate <stephenjroman@protonmail.com>
2019-04-02 23:40:59 -0400

    Add basic Spring echo web service handler

---
+++ src/main/java/echo/EchoController.java
@@ -0,0 +1,14 @@
+package echo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+
+    @RequestMapping("/")
+    public String echo(@RequestParam(value="msg", defaultValue="default val") String msg) {
+        return msg;
+    }
+}
